2288a4b70ed4b1521a9cb3eaa126c7d73475b801
Wayne Pope <waypope3@users.noreply.github.com>
2026-04-19 13:06:07 +0800

    feat: add package init with version and public API exports

--- src/timesfm/__init__.py
+++ src/timesfm/__init__.py
@@ -1,29 +1,11 @@
-# Copyright 2025 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""TimesFM: A time series forecasting model.
 
-"""TimesFM API."""
+This package provides a PyTorch-based implementation of the TimesFM
+foundation model for time series forecasting.
+"""
 
-from .configs import ForecastConfig
+from timesfm.model import TimesFM
+from timesfm.config import TimesFMConfig
 
-try:
-  from .timesfm_2p5 import timesfm_2p5_torch
-  TimesFM_2p5_200M_torch = timesfm_2p5_torch.TimesFM_2p5_200M_torch
-except ImportError:
-  pass
-
-try:
-  from .timesfm_2p5 import timesfm_2p5_flax
-  TimesFM_2p5_200M_flax = timesfm_2p5_flax.TimesFM_2p5_200M_flax
-except ImportError:
-  pass
+__version__ = "0.1.0"
+__all__ = ["TimesFM", "TimesFMConfig"]
